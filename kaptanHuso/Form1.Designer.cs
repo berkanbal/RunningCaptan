b2859c5947123f01ed44f697eb50de339b78b6ef
Berkan <bberkanbal@hotmail.com>
2024-07-30 23:14:57 +0300

    Proje dosyası ekle.

---
+++ kaptanHuso/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace kaptanHuso
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.captan = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.boostBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.captan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captan
+            // 
+            this.captan.BackColor = System.Drawing.Color.Transparent;
+            this.captan.Image = global::kaptanHuso.Properties.Resources.kaptan;
+            this.captan.Location = new System.Drawing.Point(423, 229);
+            this.captan.Name = "captan";
+            this.captan.Size = new System.Drawing.Size(107, 186);
+            this.captan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.captan.TabIndex = 0;
+            this.captan.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // boostBar
+            // 
+            this.boostBar.Location = new System.Drawing.Point(423, 479);
+            this.boostBar.Name = "boostBar";
+            this.boostBar.Size = new System.Drawing.Size(139, 23);
+            this.boostBar.TabIndex = 1;
+            this.boostBar.Value = 50;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(359, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Boost";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(975, 507);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boostBar);
+            this.Controls.Add(this.captan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyup);
+            ((System.ComponentModel.ISupportInitialize)(this.captan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox captan;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar boostBar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ kaptanHuso/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kaptanHuso
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight, boostbool;
+
+        int speed = 8;
+        int boost = 0;
+        string facing = "right";
+
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            boostBar.Minimum = 0;
+            boostBar.Maximum = 50;
+            boostBar.Value = boost;
+        }
+
+        private void gamekeydown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                captan.Image = Properties.Resources.kaptanSOL;
+                
+            }
+
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "Right";
+                captan.Image = Properties.Resources.kaptan;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                boostbool = true;
+            }
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if(goLeft == true && captan.Left > 0)
+            {
+                captan.Left -= speed;
+            }
+
+            if(goRight == true && captan.Left + captan.Width < this.ClientSize.Width)
+            {
+                captan.Left += speed;
+            }
+
+            if (boostbool == true && boost > 0)
+            {
+                speed = 12;
+                if(boost>1)
+                boost = boost - 2;
+
+            }
+            else
+            {
+                speed = 8;
+                if(boost<50)
+                    boost = boost + 1;
+            }
+            boostBar.Value = boost;
+        }
+
+        private void gamekeyup(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+                
+
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+               
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                boostbool = false;
+            }
+        }
+
+        
+
+        }
+
+        
+
+       
+    }
+
